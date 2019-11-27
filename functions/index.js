5809b58dd5ae53235927edd26a0495df722cb0f8
morch028 <morch028@uottawa.ca>
2019-11-26 23:16:13 -0500

    Adds webhook logging endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const uidFromUrl = (url) => {
+  const [, uid] = url.match(/\/api\/log\/([A-Za-z]+).*/);
+  return uid;
+};
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.logWebhook = functions.https.onRequest(async (request, response) => {
+  try {
+    const {body, method, query, originalUrl} = request;
+    const uid = uidFromUrl(originalUrl);
+    console.log(body, method, query, uid);
+    await app.firestore()
+    .collection("webhooks")
+    .doc(uid)
+    .collection("logs")
+    .add({
+      date: new Date(),
+      originalUrl,
+      method,
+      body,
+      query,
+      uid
+    });
+
+    return response
+      .status(200)
+      .send("Logged");
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(500)
+      .send("Borked it");
+  }
+
+});
